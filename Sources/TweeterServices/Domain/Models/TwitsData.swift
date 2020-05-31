74b1a42de2be2685275a1b186294218419670c1f
Víctor Jiménez <gomi88@hotmail.es>
2020-05-31 18:46:26 +0200

    Fix problem not returning value

--- Sources/TweeterServices/Domain/Models/TwitsData.swift
+++ Sources/TweeterServices/Domain/Models/TwitsData.swift
@@ -7,22 +7,23 @@ private func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
     for bundle in Bundle.allBundles {
-        guard let file = bundle.url(forResource: filename, withExtension: nil)
-            else {
-                fatalError("Couldn't find \(filename) in main bundle.")
+        if let file = bundle.url(forResource: filename, withExtension: nil) {
+            do {
+                data = try Data(contentsOf: file)
+            } catch {
+                fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                return try decoder.decode(T.self, from: data)
+            } catch {
+                fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+            }
+        } else {
+            fatalError("Couldn't find \(filename) in main bundle.")
         }
     }
     
-    do {
-        data = try Data(contentsOf: file)
-    } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
-    }
-    
-    do {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    } catch {
-        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
-    }
+    fatalError("Couldn't find \(filename) in main bundle.")
 }
